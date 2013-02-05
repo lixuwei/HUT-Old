e6991769ba04bb52a77ef15bfbba16822a774f6b
Droid <droidqw@gmail.com>
2013-02-05 16:41:08 +0800

    22

--- project/HUT/src/com/hut/action/ClassAction.java
+++ project/HUT/src/com/hut/action/ClassAction.java
@@ -2,8 +2,6 @@ package com.hut.action;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.net.URLDecoder;
-import java.net.URLEncoder;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,7 +17,6 @@ import org.apache.struts2.json.annotations.JSON;
 import com.hut.domain.Class;
 import com.hut.domain.Mid;
 import com.hut.domain.vo.ClassVO;
-import com.hut.domain.vo.TableItem;
 import com.hut.service.ClassService;
 import com.hut.service.MidService;
 import com.opensymphony.xwork2.ActionSupport;
@@ -85,61 +82,20 @@ public class ClassAction extends ActionSupport {
 		return null;
 	}
 
-	public String BuildClassTable() {
+	public String BuildClassTable() throws IOException {
 		HttpServletResponse response = ServletActionContext.getResponse();
-		List<Mid> list = new ArrayList<Mid>();
 		response.setCharacterEncoding("UTF-8");
-		PrintWriter pw = null;
-		try {
-			pw = response.getWriter();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		String[][] table = new String[4][5];
-		list = midService.getAllMids();
-		for(int i =0;i<4;i++){
-			for(int n=0;n<5;n++){
-				table[i][n]="";
-			}
-		}
+		PrintWriter pw = response.getWriter();
+		List<JSONObject> jsonList = new ArrayList<JSONObject>();
+		List<Mid> list = midService.getAllMids();
 		for(Mid mid : list){
-			int random = (int)(Math.random()*20);
-			if(table[random/5][random%5]!=""){
-				table[random/5][random%5]=table[random/5][random%5]+"<br/>"+mid.getLessonName()+"("+mid.getClassName()+","+mid.getClassRoom()+","+mid.getTeacherName()+")";
-			}else{
-				table[random/5][random%5]=mid.getLessonName()+"("+mid.getClassName()+","+mid.getClassRoom()+","+mid.getTeacherName()+")";
-			}
-			   
-			
+			int temp = (int) (Math.random()*60);
+			JSONObject jo = new JSONObject();
+			jo.accumulate("position",(temp/10+1)+"-"+(temp%10+1));
+			jo.accumulate("value", mid.getLessonName()+"&nbsp"+"("+mid.getClassName()+")<br>"+mid.getTeacherName()+"&nbsp&nbsp"+mid.getClassRoom()+"<br>");
+			jsonList.add(jo);
 		}
-		List<TableItem> tableList = new ArrayList<TableItem>();
-		for(int i = 0 ;i<5;i++){
-			TableItem temp = new TableItem();
-			temp.setItemName(""+(i+1));
-			tableList.add(temp);
-		}
-		for(int i = 0;i<5;i++){
-			for(int n = 0;n<4;n++){
-				switch(n){
-				case 0:
-					tableList.get(i).setOne(table[n][i]);
-					break;
-				case 1:
-					tableList.get(i).setTwo(table[n][i]);
-					break;
-				case 2:
-					tableList.get(i).setThree(table[n][i]);
-					break;
-				case 3:
-					tableList.get(i).setFour(table[n][i]);
-					break;
-				}
-			}
-		}
-		JSONArray ja = JSONArray.fromObject(tableList);
-		pw.print(ja);
-	
+		pw.print(jsonList);
 		return null;
 	}
 	
